d3746cd054ea8612ff5b025a516d030f4de6e049
akmurugan <47816686+akmurugan@users.noreply.github.com>
2020-11-24 15:32:01 +0530

    Update outputs.tf

--- Azure - Terraform/modules/outputs.tf
+++ Azure - Terraform/modules/outputs.tf
@@ -1,18 +1,18 @@
 ## Key-vault Output
 output "id" {
-  value = module.keyvalut.id
+  value = module.keyvalut.*.id
 }
 ## VM's Ouput
 output "vm-id" {
-   value = module.windowsvms.vm-id
+   value = module.windowsvms.*.vm-id
   }
  
 output "vm-identity-id" {
-  value = module.windowsvms.vm-identity-id
+  value = module.windowsvms.*.vm-identity-id
 }
  output  "vm-admin_username" {
-  value =  module.windowsvms.vm-admin_username
+  value =  module.windowsvms.*.vm-admin_username
 }
 output "vm-admin_password" {
-  value = module.windowsvms.vm-admin_password
+  value = module.windowsvms.*.vm-admin_password
 }
